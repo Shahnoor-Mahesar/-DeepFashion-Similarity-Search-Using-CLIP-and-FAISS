66bb4827435dea199203e87c4ffe3ba5b1fd7bf9
Shahnoor Mahesar <shahnoormahesar7@gmail.com>
2025-03-14 23:33:16 +0500

    smal update

--- DeepFashion_Cloth_Recommender/client/src/firebase.js
+++ DeepFashion_Cloth_Recommender/client/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLp-utY3igem9M0eJlEOVL8nwktoyFba8",
+  apiKey: "-",
   authDomain: "ecommerce-app-fe367.firebaseapp.com",
   projectId: "ecommerce-app-fe367",
   storageBucket: "ecommerce-app-fe367.appspot.com",
-  messagingSenderId: "590570938715",
-  appId: "1:590570938715:web:534e38eabdb44cd12afa7e",
-  measurementId: "G-CRDCGJRF8L"
+  messagingSenderId: "-",
+  appId: "-",
+  measurementId: "-"
 };
 
 // Initialize Firebase
